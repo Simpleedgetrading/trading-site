d6d300869a1d1f77c43c9055e1f0eb1bf8892cb4
Sam Dean <cfxcapital@mailfence.com>
2026-03-07 20:42:17 +0700

    connected stripe price id

---
+++ app/api/checkout/route.js
@@ -0,0 +1,24 @@
+import Stripe from "stripe"
+import { NextResponse } from "next/server"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+export async function POST() {
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "subscription",
+
+    line_items: [
+      {
+        price: "price_1T7Yq92fCu5W6YrUcqwrpOrb",
+        quantity: 1
+      }
+    ],
+
+    success_url: "https://trading-site-nine.vercel.app/success",
+    cancel_url: "https://trading-site-nine.vercel.app"
+  })
+
+  return NextResponse.json({ url: session.url })
+}
